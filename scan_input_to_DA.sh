bd40959c029029d9be2110aacfcc1104e54383d4
dpellegr <dario.pellegrini@cern.ch>
2017-07-14 10:47:27 +0200

    fixed syntax errors

--- scan_input_to_DA.sh
+++ scan_input_to_DA.sh
@@ -1,7 +1,8 @@
 
 # pass 1 to skip madx, 2 to skip sixtrack, 3 to skip check
 
-source ./sixdeskenv.sh
+source ./scan_definitions.sh
+source ./sixdeskenv
 
 entry_point=0
 if [ "$#" -ge "1" ]
@@ -19,8 +20,11 @@ if [ $entry_point -lt 2 ]; then
   $sixdesk_path/condor_wait.sh -n "run_six/$workspace/" -i 600
 fi
 
-if [ $entry_point -lt 3]; then
-  while [ cnt=$(./scan_run_check); cnt -gt 0 ] do
+if [ $entry_point -lt 3 ]; then
+  while 
+    cnt=$(./scan_run_check.sh)
+    [ $cnt -gt 0 ]
+  do
     echo "Rerunning $cnt studies"
     $sixdesk_path/condor_wait.sh -n "run_six/$workspace/" -i 600
   done
--- scan_run_check.sh
+++ scan_run_check.sh
@@ -14,11 +14,14 @@ do
   echo "####################################################"
   echo 
   
+  $sixdb_path/sixdb studies/${mask} load_dir
   $sixdb_path/sixdb ${mask}.db check_results
   if [ $? -ne 0 ]
   then
     ((++cnt))
     $sixdesk_path/run_six.sh -d $mask -a
+  else
+    echo " ### OK !"
   fi
 done
 
